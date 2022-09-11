e2ef89c5348d125153a7f7136d850f6aae83c8c0
Kleber Silva <krsmga@gmail.com>
2022-09-11 15:32:59 -0300

    Inclusion of Helper.

---
+++ AutoBattle/Helper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace AutoBattle
+{
+    /// <summary>
+    /// Helper class for reusable resources
+    /// </summary>
+    public static class Helper
+    {
+        /// <summary>
+        /// Checks if the console input is an integer value
+        /// </summary>
+        /// <param name="input">Value taken by console input.</param>
+        /// <returns>It always returns an integer value, and if the input is not valid, it returns zero. This test does not require an exception.</returns>
+        public static int GetIntParsed(string input)
+        {
+            int result;
+            Int32.TryParse(input, out result);
+            return result;
+        }
+    }
+}
+
